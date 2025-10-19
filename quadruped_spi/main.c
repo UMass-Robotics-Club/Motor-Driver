f0f06d386dd300a2a59bc7ce3e780404d8a95ced
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-10-19 18:18:21 -0400

    Added print response function and changed some content in main

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -1,21 +1,31 @@
 #include "rs02.h"
 //motor already includes jetgpio
 
+#define SPI_CHAN 0
 #define SPI_MODE 0
+
+
+int print_packet(uint8_t* packet, size_t len){
+
+    for(int i=0; i<len; i++){
+        printf("%02X ", packet[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
 
 int main(){
 
-    int handle=spiOpen(0, 5000000, 0, 0, 8, 1, 1); //SPI 1
+    int handle = spiOpen(SPI_CHAN, 5000000, SPI_MODE, 0, 8, 1, 1); //SPI 1
 
     uint8_t id = 0; //CAN_ID placeholder
-
     uint8_t rxpacket[11]; //response packet
-    
     uint8_t txpacket[11] = rs02_gen_can_header(handle, SPI_MODE, id, (uint16_t) RS02_ENABLE_DATA);
     
     rs02_jetson_spi_tx(handle, txpacket, rxpacket);
 
-    //print response packet - write a function for that(?)
+    print_packet(rxpacket, sizeof(rxpacket));
     
-    spiClose();
+    spiClose(handle);
 }
