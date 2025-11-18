89b3c953c129f0fa808f983fbeb1303769333cf4
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-17 22:35:16 -0500

    random

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -32,7 +32,7 @@ int main(){
 
 
     uint8_t id = 0; //CAN_ID placeholder
-    uint8_t rxpacket[11]; //response packet
+    uint8_t rxpacket[14]; //response packet
     uint8_t* txpacket = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
     
     //rs02_jetson_spi_tx(handle, txpacket, rxpacket);
@@ -41,7 +41,7 @@ int main(){
 
     if (err >= 0){
         fprintf(stdout, "SPI transfer okay\n");
-        print_packet(rxpacket, sizeof(rxpacket));
+        print_packet(txpacket, sizeof(txpacket));
     }
 
     else fprintf(stderr, "SPI transfer error: %d\n", err);
