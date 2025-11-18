76da580baf91fc2681ddb0d8ad3897e19c91f9f0
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-17 22:39:37 -0500

    fixed issue with txpacket length

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -33,8 +33,7 @@ int main(){
 
     uint8_t id = 0; //CAN_ID placeholder
     uint8_t rxpacket[14]; //response packet
-    uint8_t txpacket[14]; 
-    txpacket = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
+    uint8_t txpacket[14] = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
     
     //rs02_jetson_spi_tx(handle, txpacket, rxpacket);
 
