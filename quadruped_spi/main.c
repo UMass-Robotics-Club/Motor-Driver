74654be3315a4892161c876e7388d08fff205cd5
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-17 19:23:42 -0500

    corrected use of rs02 function in main.c

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -31,7 +31,7 @@ int main(){
 
     uint8_t id = 0; //CAN_ID placeholder
     uint8_t rxpacket[11]; //response packet
-    uint8_t txpacket[11] = rs02_spi_tx_packet(handle, SPI_MODE, id, RS02_ENABLE_DATA);
+    uint8_t* txpacket = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
     
     rs02_jetson_spi_tx(handle, txpacket, rxpacket);
 
