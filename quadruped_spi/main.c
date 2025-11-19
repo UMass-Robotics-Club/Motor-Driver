105dda835297c0de98bd8da92ea11f8e8d93e6cd
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-19 00:16:57 -0500

    SPI

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -44,7 +44,7 @@ int main(){
 
     usleep(1000000);
 
-    lgSpiXfer(handle, txpacket, rxpacket, 14);
+    lgSpiXfer(handle, rxpacket, txpacket, 14);
 
     if (err >= 0){
         fprintf(stdout, "SPI transfer okay\n");
