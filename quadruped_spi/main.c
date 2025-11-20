057da72fafdaaeba3bb04835b83f848303f686fd
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-19 19:32:52 -0500

    SPI

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -33,18 +33,10 @@ int main(){
 
 
     uint8_t id = 0; //CAN_ID placeholder
-    uint8_t rxdummy[14] = {0};
-    uint8_t txdummy[14] = {0};
     uint8_t* txpacket = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
     uint8_t rxpacket[14] = {0};
 
-    int err = 1;
-    
-    lgSpiXfer(handle, txpacket, rxdummy, 14);
-
-    usleep(1000000);
-
-    lgSpiXfer(handle, txdummy, rxpacket, 14);
+    int err = lgSpiXfer(handle, txpacket, rxpacket, 14);
 
     if (err >= 0){
         fprintf(stdout, "SPI transfer okay\n");
