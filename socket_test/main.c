3b241f5c4ca231ad1cfb8ebf6fdc37bcdae8e53c
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-11-19 19:37:32 -0500

    socket test

---
+++ socket_test/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+
+    // 1. Create socket
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    // 2. Server info
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);  // Remote server port
+
+    if (inet_pton(AF_INET, "192.168.1.100", &serv_addr.sin_addr) <= 0) {
+        perror("inet_pton");
+        return 1;
+    }
+
+    // 3. Connect
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        perror("connect");
+        return 1;
+    }
+
+    printf("Connected to server!\n");
+
+    // 4. Send and receive
+    const char *msg = "Hello from Raspberry Pi 5!\n";
+    send(sock, msg, strlen(msg), 0);
+
+    char buf[1024] = {0};
+    int n = recv(sock, buf, sizeof(buf)-1, 0);
+    if (n > 0) {
+        printf("Received: %s\n", buf);
+    }
+
+    close(sock);
+    return 0;
+}
