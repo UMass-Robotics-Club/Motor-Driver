929bb8292de10c524884b801eb6ac7e0307c1778
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-12-03 20:32:41 -0500

    small changes

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -29,16 +29,14 @@ int main(){
     if (chip >= 0) fprintf(stdout, "open successful\n");
 
     else fprintf(stderr, "lgpio init failed\n");
-    int handle = lgSpiOpen(0, 0, 10000000, 0);
+    int handle = lgSpiOpen(0, 0, 1000000, 0);
 
 
-    uint8_t id = 0; //CAN_ID placeholder
+    uint8_t id = 4; //CAN_ID placeholder
     uint8_t* txpacket = rs02_spi_tx_packet(0, SPI_MODE, id, RS02_ENABLE_DATA);
-    uint8_t rxpacket[14] = {0};
+    uint8_t rxpacket[14];
 
     int err = lgSpiXfer(handle, txpacket, rxpacket, 14); //send
-
-    int err = lgSpiXfer(handle, txpacket, rxpacket, 14); //receive
 
     if (err >= 0){
         fprintf(stdout, "SPI transfer okay\n");
