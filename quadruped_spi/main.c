61c7e50889a93651d57f0c0baba829820fb6219e
Siddharth <110020182+lxremipsxm@users.noreply.github.com>
2025-12-08 21:06:03 -0500

    Fixed SPI and changed code conciseness

--- quadruped_spi/main.c
+++ quadruped_spi/main.c
@@ -17,20 +17,6 @@ int print_packet(uint8_t* packet, size_t len){
     return 0;
 }
 
-int rpi5_spi_init(int chipnum, int rate, int mode){
-        /*RPI 5 LGPIO SPI setup*/
-    int chip = lgGpiochipOpen(chipnum);
-
-    if (chip >= 0) fprintf(stdout, "open successful\n");
-
-    else{
-        fprintf(stderr, "lgpio init failed\n");
-        return -1;
-    } 
-    
-
-    return lgSpiOpen(0, 0, 1000000, 0);
-}
 
 int send_packet_rpi5(int handle, uint8_t* packet, uint8_t rxBuf, size_t len){
     int err;
@@ -48,16 +34,26 @@ int main(){
     int handle = spiOpen(SPI_CHAN, 5000000, SPI_MODE, 0, 8, 1, 1); //SPI 1
     */
 
-    int handle;
+    int chip = lgGpiochipOpen(0);
 
-    if ((handle = rpi5_spi_init(0, 1000000, 0)) < 0){
+    if (chip >= 0) fprintf(stdout, "open successful\n");
+
+    else{
+        fprintf(stderr, "lgpio init failed\n");
+        return -1;
+    } 
+    
+
+    int handle;
+    
+    if ((handle = lgSpiOpen(0, 0, 1000000, 0)) < 0){
         printf("Error initializing GPIO");
     }
 
     uint8_t id = 0; //CAN_ID placeholder
 
     uint8_t rxpacket[14]; 
-    uint8_t* txpacket = rs02_spi_tx_packet(0, 1, id, RS02_ENABLE_DATA);
+    uint8_t* txpacket = rs02_spi_tx_packet(0, 1, id, RS02_ENABLE_DATA); //for output on can controller 1 with id 1 with enable command
     err = send_packet_rpi5(handle, txpacket, rxpacket, 14);
 
     if (err >= 0){
